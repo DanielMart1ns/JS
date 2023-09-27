a87c8aeb920c86796e830b525c55e0de5a6c0068
DanielMart1ns <danielmmrodrigues09@gmail.com>
2023-09-27 16:44:01 -0300

    aula de array add

---
+++ aula15/teste.js
@@ -0,0 +1,8 @@
+let num = [5,7,2,1,5]
+
+num.sort()
+num.push(0)
+console.log(`Valores: ${num}`)
+console.log(`Número de elementos: ${num.length} elementos`)
+console.log(`Primeiro valor: ${num[0]}`)
+console.log(`O valor 7 está na posição: ${num.indexOf(7)}`)
---
+++ aula15/vetor-na-tela.js
@@ -0,0 +1,23 @@
+let valores = [3,1,6,8,4,6]
+let i
+
+//console.log(valores)
+
+/*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5])
+*/
+
+/*
+for(i = 0; i <= valores.length; i++){
+    console.log(valores[i])
+}
+*/
+
+for (i in valores){
+    console.log(valores[i])
+}
